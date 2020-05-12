c784aae496a16beada7d4d79381f9e8c31f19bba
Guilherme Simas <guilherme_abinader@hotmail.com>
2020-05-12 19:19:50 -0300

    Introduzi um enum ItemType para representar o tipo de cada item. A própria regra de negócio parece já dividir a tratativa de cada item baseado em um tipo, que atualmente é identificado pelo nome do item. Introduzi um método que obtém o tipo a partir de um item, de modo a separar essa regra de negócio da tratativa da atualização de um item baseado em seu tipo.

--- python/gilded_rose/gilded_rose.py
+++ python/gilded_rose/gilded_rose.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from enum import Enum
 
 class GildedRose(object):
 
@@ -7,27 +8,28 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            item_type = ItemType.from_item(item)
+            if item_type not in (ItemType.aged_brie, ItemType.backstage_pass):
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
+                    if item_type != ItemType.legendary:
                         item.quality = item.quality - 1
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    if item_type == ItemType.backstage_pass:
                         if item.sell_in < 11:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
+            if item_type != ItemType.legendary:
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item_type != ItemType.aged_brie:
+                    if item_type != ItemType.backstage_pass:
                         if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
+                            if item_type != ItemType.legendary:
                                 item.quality = item.quality - 1
                     else:
                         item.quality = item.quality - item.quality
@@ -44,3 +46,22 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class ItemType(Enum):
+    common = 1
+    aged_brie = 2
+    backstage_pass = 3
+    legendary = 4
+    conjured = 5
+
+    @classmethod
+    def from_item(cls, item: Item):
+        if item.name == "Aged Brie":
+            return cls.aged_brie
+        if item.name == "Sulfuras, Hand of Ragnaros":
+            return cls.legendary
+        if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return cls.backstage_pass
+        if item.name == "Conjured":
+            return cls.conjured
+        return cls.common
