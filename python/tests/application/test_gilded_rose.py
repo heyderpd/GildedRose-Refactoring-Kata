451efa071a5f12f087b447570a51ad005f383db8
Guilherme Simas <guilherme_abinader@hotmail.com>
2020-05-12 18:58:20 -0300

    Add current functionalities test cases

--- python/tests/application/test_gilded_rose.py
+++ python/tests/application/test_gilded_rose.py
@@ -6,8 +6,80 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(BaseApplicationTest):
 
-    def test_whenUpdatingZeroQualityItem_qualityShouldNotDecrease(self):
-        item = Item("foo", 0, 0)
+    def test_whenCommonItem_qualityAndSellinShouldDecrease(self):
+        item = Item("common", 10, 10)
         gilded_rose = GildedRose([item])
         gilded_rose.update_quality()
-        self.assertItemEqual(item, quality=0, name="foo", sell_in=-1)
+        self.assertItemEqual(item, name="common", sell_in=9, quality=9)
+
+    def test_whenPastSellInDate_qualityShouldDecreaseTwiceAsFast(self):
+        item = Item("common", -1, 10)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, name="common", sell_in=-2, quality=8)
+
+    def test_whenZeroQualityItem_qualityShouldNotDecrease(self):
+        item = Item("common", 0, 0)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, name="common", sell_in=-1, quality=0)
+
+    def test_whenAgedBrie_qualityShouldIncrease(self):
+        item = Item("Aged Brie", 1, 1)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=0, quality=2)
+
+    def test_whenAgedBriePastSellInDate_qualityShouldIncreaseTwiceAsFast(self):
+        item = Item("Aged Brie", 0, 1)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=-1, quality=3)
+
+    def test_whenAgedBrieIsMaximunQuality_qualityShouldNotIncrease(self):
+        item = Item("Aged Brie", 0, 50)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=-1, quality=50)
+
+    def test_whenAgedBrieIsMaximunQuality_qualityShouldNotIncrease(self):
+        item = Item("Aged Brie", 0, 50)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=-1, quality=50)
+
+    def test_whenSulfuras_qualityAndSellInShouldNotDecrease(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 10, 10)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=10, quality=10)
+
+    def test_whenSulfurasPastSellInDate_qualityAndSellinShouldNotDecrease(self):
+        item = Item("Sulfuras, Hand of Ragnaros", 0, 10)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=0, quality=10)
+
+    def test_whenBackstagePassMoreThan10DaysLeft_qualityShouldIncreaseBy1(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 11, 1)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=10, quality=2)
+
+    def test_whenBackstagePass10DaysLeftOrLess_qualityShouldIncreaseBy2(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 10, 1)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=9, quality=3)
+
+    def test_whenBackstagePass5DaysLeftOrLess_qualityShouldIncreaseBy3(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 5, 1)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=4, quality=4)
+
+    def test_whenBackstagePassPastSellInDate_qualityShouldZero(self):
+        item = Item("Backstage passes to a TAFKAL80ETC concert", 0, 5)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertItemEqual(item, sell_in=-1, quality=0)
