044971accf19963320d1d716d934881bddb1a794
heyder.dias <heyder.dias@zoop.com.br>
2020-05-26 00:24:27 -0300

    reset solution

--- python/gilded_rose/gilded_rose.py
+++ python/gilded_rose/gilded_rose.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
 from typing import List
 
-from gilded_rose.entities import Item, ItemType, ItemRunner
-from gilded_rose.entities.constants import MAXIMUM_QUALITY
-from gilded_rose.factories.item import ItemRunnerFactory
+from gilded_rose.update import update
 
-
-class GildedRose:
-
-    items: List[Item]
-    item_wrappers: List[ItemRunner]
+class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
-        self.runners = [ItemRunnerFactory.from_item(it) for it in items]
 
     def update_quality(self):
-        for runner in self.runners:
-            runner.update()
+        for item in self.items:
+            update(item)
