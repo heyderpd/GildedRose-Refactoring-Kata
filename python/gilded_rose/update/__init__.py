f7c6112b083f46ca17b4dfb8db2646d517a576d4
heyder.dias <heyder.dias@zoop.com.br>
2020-05-26 00:32:07 -0300

    use compose and import operators

--- python/gilded_rose/update/__init__.py
+++ python/gilded_rose/update/__init__.py
@@ -1,40 +1,41 @@
 from craftsmen import identity, curry, placeholder, p, compose, rcompose, cmap, map
 
-item_behaviour = {
-  ItemType.legendary: [],
+from .operators import IncreaseQuality, DecreaseQuality, CapQualityOnMaximum, QualityIsNeverNegative, SetQualityTo, WhenPastExpiration, WhenSellInPast, UpdateSellInDate
 
-  ItemType.maturable: [
-    IncreaseQuality(),
-    UpdateSellInDate(),
-    WhenPastExpiration(IncreaseQuality()),
-    CapQualityOnMaximum(),
-    QualityIsNeverNegative(),
-  ],
 
-  ItemType.common: [
-    DecreaseQuality(),
-    UpdateSellInDate(),
-    WhenPastExpiration(DecreaseQuality()),
-    QualityIsNeverNegative(),
-  ],
+legendary = identity
 
-  ItemType.backstage_pass: [
-    IncreaseQuality(),
-    WhenSellInPast(11, IncreaseQuality()),
-    WhenSellInPast(6, IncreaseQuality()),
-    UpdateSellInDate(),
-    WhenPastExpiration(SetQualityTo(0)),
-    CapQualityOnMaximum(),
-    QualityIsNeverNegative(),
-  ],
+maturable = compose([
+  IncreaseQuality(),
+  UpdateSellInDate(),
+  WhenPastExpiration(IncreaseQuality()),
+  CapQualityOnMaximum(),
+  QualityIsNeverNegative(),
+])
 
-  ItemType.conjured: [
-    DecreaseQuality(2),
-    UpdateSellInDate(),
-    WhenPastExpiration(DecreaseQuality(2)),
-    QualityIsNeverNegative(),
-  ],
-}
+common = compose([
+  DecreaseQuality(),
+  UpdateSellInDate(),
+  WhenPastExpiration(DecreaseQuality()),
+  QualityIsNeverNegative(),
+])
+
+backstage_pass = compose([
+  IncreaseQuality(),
+  WhenSellInPast(11, IncreaseQuality()),
+  WhenSellInPast(6, IncreaseQuality()),
+  UpdateSellInDate(),
+  WhenPastExpiration(SetQualityTo(0)),
+  CapQualityOnMaximum(),
+  QualityIsNeverNegative(),
+])
+
+conjured = compose([
+  DecreaseQuality(2),
+  UpdateSellInDate(),
+  WhenPastExpiration(DecreaseQuality(2)),
+  QualityIsNeverNegative(),
+])
 
 def update(self, item):
   pass
---
+++ python/gilded_rose/update/operators.py
@@ -0,0 +1,23 @@
+def IncreaseQuality():
+  pass
+
+def DecreaseQuality():
+  pass
+
+def CapQualityOnMaximum():
+  pass
+
+def QualityIsNeverNegative():
+  pass
+
+def SetQualityTo():
+  pass
+
+def WhenPastExpiration():
+  pass
+
+def WhenSellInPast():
+  pass
+
+def UpdateSellInDate():
+  pass
