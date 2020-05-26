be0744be5a96a2477d29233e6495055ef90bbf54
heyder.dias <heyder.dias@zoop.com.br>
2020-05-26 03:54:06 -0300

    create item_behaviour, getIdentifier and update

---
+++ python/gilded_rose/src/update_item.py
@@ -0,0 +1,28 @@
+from craftsmen import identity, rcompose
+
+from .entities import constants
+from .behaviours import maturable, legendary, backstage_pass, conjured, common
+
+
+item_behaviour = {
+  constants.MATURABLE: maturable,
+  constants.LEGENDARY: legendary,
+  constants.BACKSTAGE_PASSES: backstage_pass,
+  constants.CONJURED: conjured,
+}
+
+def getIdentifier(item):
+
+  if 'Sulfuras' in item.name:
+    return constants.LEGENDARY
+  if item.name == 'Aged Brie':
+    return constants.MATURABLE
+  if 'Backstage passes' in item.name:
+    return constants.BACKSTAGE_PASSES
+  if 'Conjured' in item.name:
+    return constants.CONJURED
+
+def update(item):
+  item_type = getIdentifier(item)
+  processors = item_behaviour.get(item_type, common)
+  return processors(item)
