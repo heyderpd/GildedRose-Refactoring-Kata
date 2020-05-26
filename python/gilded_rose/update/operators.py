113aee72e7b50aaa8e3c74cfabac873460a96ecd
heyder.dias <heyder.dias@zoop.com.br>
2020-05-26 02:18:33 -0300

    return item on processor

--- python/gilded_rose/update/operators.py
+++ python/gilded_rose/update/operators.py
@@ -6,20 +6,25 @@ def __setQuality(value, item):
 
 def __sumValueWithQuality(value, item):
   __setQuality(item.quality + value, item)
+  return item
 
 def __increaseQuality(value, item):
   __sumValueWithQuality(value, item)
+  return item
 
 def __decreaseQuality(value, item):
   __sumValueWithQuality(-1 * value, item)
+  return item
 
 def __setTopLimit(limit, item):
   if item.quality > limit:
     __setQuality(limit, item)
+  return item
 
 def __setLowerLimit(limit, item):
   if item.quality < limit:
     __setQuality(limit, item)
+  return item
 
 
 def __setSellIn(value, item):
@@ -28,16 +33,19 @@ def __setSellIn(value, item):
 def __whenPastExpiration(processor, item):
   limit = 0
   WhenSellInPast(limit, processor, item)
+  return item
 
 def __whenSellInPast(limit, processor, item):
   if item.sell_in < limit:
     processor(item)
+  return item
 
 def __updateSellInDate(item):
-  __setSellIn(-1, item)
+  __setSellIn(item.sell_in  -1, item)
+  return item
 
 
-IncreaseQuality = curry(__increaseQuality)(p)
+IncreaseQuality = curry(__increaseQuality)
 
 DecreaseQuality = curry(__decreaseQuality)
 
